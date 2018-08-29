94a27baaf6410c13a5e4a37f10b8ba00a5ad50fa
EversonFeltrin <efeltrin@inf.ufsm.br>
2018-08-29 20:14:14 -0300

    Conectando sql no Postgres

--- src/java/dao/ConectaDB.java
+++ src/java/dao/ConectaDB.java
@@ -1,21 +1,60 @@
 package dao;
 
+
+//classes necessaris para uso de BD
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ConectaDB {
-    private static final String url = "";
-    private static final String user = "";
-    private static final String pass = "";
     private static final String banco = "mongo";
     
     public static void main (String args[]){
         //vai testar conforme a definição do nome do banco qual conexão levantar
+        new ConectaDB().getConexaoMySql();
         if (banco.equals("mongo")){
+            
+            //new ConectaDB().read(2);
             System.out.println("Conectado ao MongoDB");
         }
         else {
+            
             System.out.println("Conectado ao MySqlDB");
         }
         
     }
+    
+    
+    
+    public Connection getConexaoMySql(){
+        Connection conn = null;
+        String serverName = "localhost:5432"; //caminho do servidor do BD
+        String mydatabase = "pgbd2018";     //nome do BD
+        String url = "jdbc:postgresql://" + serverName + "/" + mydatabase;
+        String username = "postgres";        //nome de usuário de seu BD
+        String password = "1234";        //senha de acesso ao BD
+        
+        try{
+            String driverName = "org.postgresql.Driver";
+            Class.forName(driverName);
+            conn = DriverManager.getConnection(url, username, password);
+            if (conn != null){
+                System.out.println("conectou");
+            }else {
+                System.out.println("não conectou");
+            }
+        } catch(ClassNotFoundException ex){ //Driver não encontrado
+            ex.printStackTrace();
+        } catch(SQLException e){  //Não conecta BD
+            e.printStackTrace();
+        }
+        return conn;
+        
+    }
+    
+    
     
     
     
