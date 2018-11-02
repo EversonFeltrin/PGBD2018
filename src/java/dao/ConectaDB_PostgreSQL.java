e1dc0158858a930277aa255a5da3bf5d67278928
Cássio Araujo <caraujo@inf.ufsm.br>
2018-11-02 12:40:30 -0300

    Adicionando a conexao com o PostgreSQL e a classe DAO de cada uma das tabelas do banco.

---
+++ src/java/dao/ConectaDB_PostgreSQL.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author cassi
+ */
+public class ConectaDB_PostgreSQL {
+
+   String serverName = "localhost:5432"; //caminho do servidor do BD
+   String mydatabase = "TrabPGBD";     //nome do BD
+   String url = "jdbc:postgresql://" + serverName + "/" + mydatabase;
+   String username = "postgres";        //nome de usuário de seu BD
+   String password = "123456";        //senha de acesso ao BD
+
+   public static void main(String args[]) {
+      new ConectaDB_PostgreSQL().getConexao();
+   }
+
+   public Connection getConexao() {
+      Connection conn = null;
+      try {
+         String driverName = "org.postgresql.Driver";
+         Class.forName(driverName);
+         conn = DriverManager.getConnection(url, username, password);
+         if (conn != null) {
+            System.out.println("conectou PostgreSQL");
+         } else {
+            System.out.println("não conectou PostgreSQL");
+         }
+      } catch (ClassNotFoundException ex) { //Driver não encontrado
+         ex.printStackTrace();
+      } catch (SQLException e) {  //Não conecta BD
+         e.printStackTrace();
+      }
+      return conn;
+   }
+}
