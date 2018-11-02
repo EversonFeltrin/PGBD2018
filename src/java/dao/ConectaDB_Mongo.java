c9b995a4cc1c8ea52161e05593e4ba0e83e862f0
EversonFeltrin <efeltrin@inf.ufsm.br>
2018-11-02 16:57:46 -0300

    com a biblioteca do mongo no projeto

---
+++ src/java/dao/ConectaDB_Mongo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import org.omg.CORBA.portable.UnknownException;
+
+
+
+
+/**
+ *
+ * @author efelt
+ */
+public class ConectaDB_Mongo {
+    
+    public static void main (args[])throws UnknownException, MongoException{
+        MongoClient 
+    } 
+}
