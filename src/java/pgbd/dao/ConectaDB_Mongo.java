59ca0d9359616538984301c6e06da62b571dcdb9
EversonFeltrin <efeltrin@inf.ufsm.br>
2018-11-02 23:32:25 -0300

    create, read, update e delete no mongo implementados, ver como dividir a conexão e as consultas

--- src/java/pgbd/dao/ConectaDB_Mongo.java
+++ src/java/pgbd/dao/ConectaDB_Mongo.java
@@ -5,6 +5,7 @@
  */
 package pgbd.dao;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -12,6 +13,8 @@ import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
 import com.mongodb.ConnectionString;
+import com.mongodb.DBObject;
+import java.util.List;
 import pgbd.model.Alunos;
 
 
@@ -33,12 +36,31 @@ public class ConectaDB_Mongo {
             DBCollection collection = db.getCollection("alunos");
             
             DBCursor cursor = collection.find();
-            BasicDBObject document = new BasicDBObject();
             
-            document.put("nome","Camilla Bordin Portilho");
-            document.put("matricula","2013207542");
-            collection.insert(document);
+            BasicDBObject comparaQuery = new BasicDBObject("nome", "Camilla Bordin Portilho");
+                
+            //UPDATE ou INSERT
+            //cria o array de atividades para linkar ao usuario
+            List<Object> atividadesDBList = new BasicDBList();
+            DBObject atividadeDBObject = new BasicDBObject();
             
+            atividadeDBObject.put("classificacao","teste a mais");
+            atividadeDBObject.put("localAtividade", "São Paulo");
+            atividadeDBObject.put("dataIni", "20/10/2018");
+            atividadeDBObject.put("dataFim", "22/10/2018");
+            atividadeDBObject.put("cargaHoraria", "50");
+            atividadeDBObject.put("atividadeDesenvolvida", "Ouvinte");
+            atividadeDBObject.put("dataSubmissao", "02/11/2018");
+            atividadesDBList.add(atividadeDBObject);
+ 
+            //insere alunos no banco de dados
+            BasicDBObject aluno = new BasicDBObject();
+            aluno.put("nome","Camilla Bordin Portilho");
+            aluno.put("matricula","2013207542");
+            aluno.put("atividade",atividadesDBList);
+            
+            
+            collection.update(comparaQuery, aluno);
             
             int i = 1;
             
@@ -49,8 +71,26 @@ public class ConectaDB_Mongo {
             }
             
             
+            //DELETE
+            BasicDBObject deleteQuery = new BasicDBObject();
+            deleteQuery.put("nome","Evandro");
             
+            //DBCursor cursor;
+            cursor = collection.find(deleteQuery);
             
+            while(cursor.hasNext()){
+                DBObject registro = cursor.next();
+                collection.remove(registro);
+            }
+             
+            //int i = 1;
+            /*
+            while (cursor.hasNext()){
+                System.out.println("Documento Inserido: " + i);
+                System.out.println(cursor.next());
+                i++;
+            }
+            */
         }catch(Exception e){
             System.out.println("e");
         }
